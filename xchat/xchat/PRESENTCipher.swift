51ac74acb9ad8d0303b71c5de500a26907962f84
Nguyen Thinh <ThinhND@ThinhND.local>
2017-03-17 11:02:34 +0700

    done encryption

--- xchat/xchat/PRESENTCipher.swift
+++ xchat/xchat/PRESENTCipher.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 //      0   1   2   3   4   5   6   7   8   9   a   b   c   d   e   f
-let Sbox:[Int64] = [0xc,0x5,0x6,0xb,0x9,0x0,0xa,0xd,0x3,0xe,0xf,0x8,0x4,0x7,0x1,0x2]
-let Sbox_inv:[Int64] = [5, 14, 15, 8, 12, 1, 2, 13, 11, 4, 6, 3, 0, 7, 9, 10]
-let PBox:[Int64] = [0,16,32,48,1,17,33,49,2,18,34,50,3,19,35,51,
+let Sbox:[UInt64] = [0xc,0x5,0x6,0xb,0x9,0x0,0xa,0xd,0x3,0xe,0xf,0x8,0x4,0x7,0x1,0x2]
+let Sbox_inv:[UInt64] = [5, 14, 15, 8, 12, 1, 2, 13, 11, 4, 6, 3, 0, 7, 9, 10]
+let PBox:[UInt64] = [0,16,32,48,1,17,33,49,2,18,34,50,3,19,35,51,
                     4,20,36,52,5,21,37,53,6,22,38,54,7,23,39,55,
                     8,24,40,56,9,25,41,57,10,26,42,58,11,27,43,59,
                     12,28,44,60,13,29,45,61,14,30,46,62,15,31,47,63]
-let PBox_inv:[Int64] = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 1, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61, 2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58, 62, 3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63]
+let PBox_inv:[UInt64] = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 1, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61, 2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58, 62, 3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63]
 
 struct Int80 {
     var strKey : String
@@ -24,9 +24,12 @@ struct Int80 {
     }
     
 
-    func extract64BitKey() -> Int64 {
+    func extract64BitKey() -> UInt64 {
         let index = self.strKey.index(self.strKey.startIndex, offsetBy: 64)
-        return strtoll(self.strKey.substring(to: index), nil, 2)
+        let str = self.strKey.substring(to: index)
+        
+        let i = strtoull(str, nil, 2)
+        return i
     }
     
     mutating func getNextKeyAtRound(index: Int) {
@@ -70,13 +73,14 @@ struct Int80 {
 class PRESENTCipher: NSObject {
     
     let rounds:Int = 32
-    var roundkeys:[Int64] = []
+    var roundkeys:[UInt64] = []
     
     init?(key:String?) {
+        super.init()
         if let key = key {
             if key.characters.count * 8 == 80 {
                 //80 bit
-                
+                self.roundkeys = self.generateRoundKey80Bit(key: key)
             } else if key.characters.count * 8 == 128 {
                 //128 bit
             } else {
@@ -88,7 +92,51 @@ class PRESENTCipher: NSObject {
         
     }
     
-    func generateRoundKey80Bit(key:String) -> [Int64] {
+    func encrypt(block:String) -> String {
+        // block is 8byte String (length is 8)
+        var state = self.stringToNumber(str: block)
+        for i in 0...self.rounds - 2 {
+            var bin : String
+            state = self.addRoundKey(state: state, key: self.roundkeys[i])
+            bin = String(self.roundkeys[i], radix: 2)
+            state = self.sBoxLayer(state: state)
+            bin = String(state, radix: 2)
+            state = self.pPlayer(state: state)
+            bin = String(state, radix: 2)
+            print("")
+        }
+        let cipher = self.addRoundKey(state: state, key: self.roundkeys[self.rounds - 1])
+        return self.numberToString(num: cipher)
+    }
+    
+    func addRoundKey(state:UInt64, key:UInt64) -> UInt64 {
+        return state ^ key
+    }
+    
+    func sBoxLayer(state:UInt64) -> UInt64 {
+//        SBox function for encryption
+//        
+//        Input:  64-bit integer
+//        Output: 64-bit integer
+        
+        var output:UInt64 = 0
+        for i in 0...15 {
+            let idx = (state >> UInt64(i*4)) & 0xF
+            output += Sbox[Int(idx)] << UInt64(i*4)
+            let bin = String(output, radix: 2)
+        }
+        return output
+    }
+    
+    func pPlayer(state:UInt64) -> UInt64 {
+        var output:UInt64 = 0
+        for i in 0...63 {
+            output += ((state >> UInt64(i)) & 0x01) << PBox[i]
+        }
+        return output
+    }
+    
+    func generateRoundKey80Bit(key:String) -> [UInt64] {
 //        Generate the roundkeys for a 80-bit key
 //        
 //        Input:
@@ -96,21 +144,25 @@ class PRESENTCipher: NSObject {
 //        rounds: the number of rounds as an integer
 //        Output: list of 64-bit roundkeys as integers
         
-        var subKeys:[Int64] = []
+        var subKeys:[UInt64] = []
         var keyInt80 = Int80.init(str: key)
         
         for i in 1...self.rounds {
             subKeys.append(keyInt80.extract64BitKey())
-            
             keyInt80.getNextKeyAtRound(index: i)
         }
-        return [12]
+        return subKeys
     }
     
-    func stringToNumber(str:String) -> Int64 {
+    func stringToNumber(str:String) -> UInt64 {
         let encode = str.utf8.map{ $0 }.reduce("") { $0 + String($1, radix: 16, uppercase: false) }
         let intValue = strtoll(encode, nil, 16)
-        return intValue
+        return UInt64(intValue)
+    }
+    
+    func numberToString(num:UInt64) -> String {
+        let bin = String(num, radix: 16)
+        return bin
     }
 
 }
--- xchat/xchat/ViewController.swift
+++ xchat/xchat/ViewController.swift
@@ -16,14 +16,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
 
 //        let test = String("a".data(using: .ascii), radix:16)
-        let baseStr = "asdasdasd"
-        let encode = baseStr.utf8.map{ $0 }.reduce("") { $0 + String($1, radix: 16, uppercase: false) }
+//        let baseStr = "asdasdasd"
+//        let encode = baseStr.utf8.map{ $0 }.reduce("") { $0 + String($1, radix: 16, uppercase: false) }
 //        let x = Int64(encode, radix: 16) // Convert from Hex string
         let present = PRESENTCipher(key: "1234567890")
-        let str = "a"
-        
-        let intValue = strtoll(encode, nil, 16)
-        present?.generateRoundKey80Bit(key: "1234567890")
+        let cipherText = present?.encrypt(block: "abcd1234")
 
     }
 
